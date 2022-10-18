42afe96f4a95eb00041ce24e791a5e0949e5f932
Sarah Beckett <sarah.beckett@sky.uk>
2022-10-18 14:23:18 +0100

    updating so that treats are independent

--- dogApp/dogapp/src/components/Favourite.jsx
+++ dogApp/dogapp/src/components/Favourite.jsx
@@ -4,14 +4,37 @@ import Treats from "./Treats";
 //destructuring props below (although there's only one! Means you can use {name} instead of {props.name})
 export default function Favourite({name}) {
     const [treat, setTreat] = useState(0);
+    const [dougTreat, setDougTreat] = useState(0);
+    const [odieTreat, setOdieTreat] = useState(0);
+
+    console.log(odieTreat);
+    console.log(dougTreat);
 
     return (
         <>
         <p>This is the Favourite component, it receives state data about which your favourite dog is from the app as props but it sets its own state for treats</p>
         <p>Your favourite dog is... {name}</p>
-        <button onClick={() => setTreat(treat + 1)}>Click to give {name} a treat</button>
-        <p>{name} has {treat} treats</p>
-        <Treats number={treat} />
+        {name == "Doug" &&
+        <div>
+        <button onClick={() => setDougTreat(dougTreat + 1)}>Click to give {name} a treat</button>
+        <p>{name} has {dougTreat} treats</p>
+        </div>
+        }
+        {name == "Odie" &&
+        <div>
+        <button onClick={() => setOdieTreat(odieTreat + 1)}>Click to give {name} a treat</button>
+        <p>{name} has {odieTreat} treats</p>
+        </div>
+        }
+        <Treats number={(name+"Treat")} />
         </>
+
+        // <>
+        // <p>This is the Favourite component, it receives state data about which your favourite dog is from the app as props but it sets its own state for treats</p>
+        // <p>Your favourite dog is... {name}</p>
+        // <button onClick={() => setTreat(treat + 1)}>Click to give {name} a treat</button>
+        // <p>{name} has {treat} treats</p>
+        // <Treats number={treat} />
+        // </>
     )
 }
